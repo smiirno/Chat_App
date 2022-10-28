22a47b834c1a2bbb1bcd8bf1d7cb4252a822011d
smiirno <Arseny.Smirnov@urfu.me>
2022-10-28 14:59:11 +0500

    Add redux-persist for localStorage

--- chat_app/src/store/store.ts
+++ chat_app/src/store/store.ts
@@ -1,15 +1,38 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import currentUserReducer from './reducers/user_slice';
 import chatsReducer from './reducers/chats_slice';
 
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
 const rootReducer = combineReducers({
     currentUserReducer, chatsReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const setupStore = () => {
     return configureStore({
-        reducer: rootReducer
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                },
+            })
     })
 }
 
